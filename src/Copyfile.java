dccf2b5cc030e2ca4090ac2777df17a255fc4f78
frankzhang <frankzhang945@gmail.com>
2016-11-19 19:40:44 -0500

    finish copy file ; gonna move all file operations in same file called fileoperation.java

--- src/Copyfile.java
+++ src/Copyfile.java
@@ -2,8 +2,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.channels.FileChannel;
-
+import java.nio.file.AccessDeniedException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 public class Copyfile {
 	
 	
@@ -31,16 +37,45 @@ public class Copyfile {
 	    }
 	}
 	
+	  // put the path in the name to creat a file in taht directory append the dependecent as well
+//	createFile("E:\\testtttt");
+//	
+//	File file = new File("D:\\test.txt");
+//	File file2 = new File("E:\\testtttt");
+//	copyFile(file,file2);
+
+	
+	  //TODO: new exception
+	  public static void createFile(String filename) {
+		  
+//		  	String filename = "test";
+		    File file = new File(filename);
+
+		    if (file.exists()) {
+//		      throw new FileAlreadyExistsException(filename + " already exists.");
+		    }
+		    
+		    try {
+		      file.createNewFile();
+		    } catch (IOException e) {
+//		      throw new AccessDeniedException("Insufficient permissions to create " + filename);
+		    }
+		    
+		    // this is highly unlikely since we were able to create the file,
+		    // but doesn't hurt to check.
+		    if (!file.exists() || !file.canWrite()) {
+		      file.delete();
+//		      throw new AccessDeniedException("Insufficient permissions to write to " + filename);
+		    }
+		  }
+	
+	
+
+	
+	
+	
 	public static void main(String [ ] args) throws IOException{
-		File file = new File("D:\\test.txt");
-//		System.out.println(file.exists());
-		File file2 = new File("E:\\c.txt");
-		System.out.println(file2.exists());
-		System.out.println(file2.getAbsolutePath());
-		
-		copyFile(file,file2);
-//		 FileChannel destination = null;
-//		 destination = new FileOutputStream(file2).getChannel();
+
 		
 		
 		
